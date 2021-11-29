9559b8be4faf26f2825dd501ec6fb0073c688bee
mori801 <moritzreinmoeller@unitybox.de>
2021-11-29 14:46:49 +0100

    create template

---
+++ UdemyCpp_Template-main/1_CppGrundlagen/char.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    char myCharacter1 = 'A';
+    char offset = 2;
+
+    std::cout << myCharacter1 << std::endl;
+
+    return 0;
+}
---
+++ UdemyCpp_Template-main/1_CppGrundlagen/variables1.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x = 10;
+    double ergebnis_multi = x * x;
+
+    std::cout << ergebnis_multi;
+
+    return 0;
+}
